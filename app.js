7ad348d9441191bdf9831adb1e142c2bcf2116dc
piyakker <ericlien86@gamil.com>
2022-10-11 17:34:42 +0800

    download mongoose & set port to 3000

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
 })
 
-app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`)
+app.listen(3000, () => {
+  console.log('Listening on http://localhost:3000')
 })
